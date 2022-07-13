7d76643c7ec8c6022a4045102188e641c9d3734a
Johnson Denen <johnson.denen@gmail.com>
2022-07-13 10:54:44 -0400

    Update setup script

--- setup.sh
+++ setup.sh
@@ -4,11 +4,13 @@ export ASDF_PLUGINS=(
     direnv
     elixir
     erlang
+    neovim
     nodejs
+    python
+    rust
 )
 
 export BREW_CASKS=(
-    d12frosted/emacs-plus
     homebrew/cask-fonts
 )
 
@@ -16,9 +18,8 @@ export BREW_PKGS=(
     asdf
     autoconf
     cmake
+    ddgr
     docker
-    editorconfig
-    emacs-plus@28 --with-no-titlebar --with-modern-sexy-v2-icon
     fd
     font-hasklig
     font-source-code-pro
@@ -26,6 +27,7 @@ export BREW_PKGS=(
     gnutls
     gpg
     iterm2
+    make
     pinentry-mac
     pre-commit
     ripgrep
@@ -33,7 +35,6 @@ export BREW_PKGS=(
     wxmac
 )
 
-# PACKAGE INSTALLATION
 /usr/bin/env bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 for cask in ${BREW_CASKS[@]}; do
@@ -46,34 +47,18 @@ for plugin in ${ASDF_PLUGINS[@]}; do
     asdf plugin add $plugin
 done
 
-git clone https://github.com/jdenen/config ~/Code
-cd ~/Code
+curl -L git.io/antigen > ~/antigen.zsh
+cp dot/* ~
+cp -r bin ~/Code
+cp -r iex ~/Code
+chmod u+x ~/Code/bin/*
 
-# EMACS
-git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
-~/.emacs.d/bin/doom install
-~/.emacs.d/bin/org-tangle emacs.org
-~/.emacs.d/bin/doom sync
+asdf install
 
-# GIT
-~/.emacs.d/bin/org-tangle git.org
+bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+mkdir -p ~/.config/lvim/
+cp config.lua ~/.config/lvim/
+
 # TODO
 # key=$(gpg --list-secret-keys --keyid-format LONG | grep -e "^sec" | cut -d'/' -f2 | cut -d' ' -f1)
 # git config --global user.signingkey $key
-
-# ELIXIR
-~/.emacs.d/bin/org-tangle elixir.org
-
-git clone https://github.com/elixir-lsp/elixir-ls ~/.elixir_ls
-(
-    cd ~/.elixir_ls
-    git checkout $(git tag --sort=committerdate | head -1)
-    cp .release-tool-versions .tool-versions
-    asdf install
-    mix do local.hex --force, local.rebar --force, deps.get, compile, elixir_ls.release -o release
-)
-
-# ZSH
-curl -L git.io/antigen > ~/antigen.zsh
-~/.emacs.d/bin/org-tangle zsh.org
-chmod u+x ~/Code/bin/*
