1e45c5c18581bd804311155bff99eed094fb095d
Johnson Denen <johnson.denen@gmail.com>
2022-08-11 23:03:37 -0400

    Rewrite setup script

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Configuration for my MacOS development environment:
 + [Oh My Zsh](https://ohmyz.sh)
 + Git with GPG signing
-+ [Elixir](https://elixir-lang.org), Erlang, NodeJS managed by `asdf`
++ [Elixir](https://elixir-lang.org) and Erlang managed by `asdf`
 + [LunarVim](https://lunarvim.org)
 
 ## Setup
--- setup.sh
+++ setup.sh
@@ -1,70 +1,69 @@
 #!/usr/bin/env bash
 
-export ASDF_PLUGINS=(
-    direnv
-    elixir
-    erlang
-    neovim
-    nodejs
-    python
-    rust
-)
+function _install_homebrew {
+  /usr/bin/env bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/johnsondenen/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+}
 
-export BREW_CASKS=(
-    homebrew/cask-fonts
-)
+function _tap_brew_casks {
+  brew tap homebrew/cask-fonts
+}
 
-export BREW_PKGS=(
-    asdf
-    autoconf
-    cmake
-    ddgr
-    docker
-    fd
-    font-hasklig
-    git
-    gnutls
-    gnupg
-    k9s
-    lazygit
-    make
-    pinentry-mac
-    pre-commit
-    ripgrep
-    sd
-    wxmac
-)
-
-/usr/bin/env bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/johnsondenen/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
-
-for cask in ${BREW_CASKS[@]}; do
-    brew tap $cask
-done
-
-brew install $BREW_PKGS
-
-for plugin in ${ASDF_PLUGINS[@]}; do
+function _install_asdf_plugins {
+  for plugin in direnv elixir erlang neovim nodejs python rust; do
     asdf plugin add $plugin
-done
+  done
+}
 
-mkdir -p ~/Code && cd ~/Code
-git clone https://github.com/jdenen/config && cd config
+function _install_brew_packages {
+  brew install \
+    asdf autoconf cmake ddgr docker fd font-hasklig git gnutls gnupg k9s \
+    lazygit make pinentry-mac pre-commit ripgrep sd wxmac
+}
 
-chmod u+x bin
-for f in $(ls -a dot/.*); do cp $f ~; done
+function _get_antigen {
+  curl -L git.io/antigen > ~/antigen.zsh
+}
 
-curl -L git.io/antigen > ~/antigen.zsh
+function _clone_repo {
+  git clone https://github.com/jdenen/config ~/Code/config
+}
 
-asdf install
+function _setup_zsh {
+  chmod u+x ~/Code/config/bin
+  for f in $(ls -a ~/Code/config/bin/dot/.*); do cp $f ~; done
+}
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+function _install_nvm {
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+  nvm use system || nvm install --lts && nvm use --lts
+}
 
-bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
-mkdir -p ~/.config/lvim/
-cp config.lua ~/.config/lvim/
+function _asdf_install {
+  (
+    cd ~/Code/config/dot
+    asdf install
+  )
+}
 
-# TODO
+function _isntall_lvim {
+  bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+  mkdir -p ~/.config/lvim/
+  cp ~/Code/config/config.lua ~/.config/lvim/
+}
+
+_install_homebrew
+_tap_brew_casks
+_install_brew_packages
+_install_asdf_plugins
+_get_antigen
+_clone_repo
+_setup_zsh
+_install_nvm
+_asdf_install
+_install_lvim
+
+# TODO: Setup GPG
 # key=$(gpg --list-secret-keys --keyid-format LONG | grep -e "^sec" | cut -d'/' -f2 | cut -d' ' -f1)
 # git config --global user.signingkey $key
