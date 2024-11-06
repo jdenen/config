1d5ee4df6910f8df0d3aec6873e65a94dc428473
Johnson Denen <johnson.denen@gmail.com>
2024-11-06 16:26:47 -0500

    Tweak setup script

--- setup.sh
+++ setup.sh
@@ -4,23 +4,19 @@ BREW_PACKAGES=(
   asdf
   cmake
   curl
-  ddgr
   docker
   fd
   font-hasklig
   fop
+  fzf
   git
   gnutls
   gnupg
   gum
-  k9s
+  helix
   lazygit
-  libxslt
   make
-  minikube
-  openssl@1.1
-  pinentry-mac
-  pre-commit
+  openssl@3
   ripgrep
   sd
   tmux
@@ -40,7 +36,7 @@ function _tap_brew_casks {
 }
 
 function _install_asdf_plugins {
-  for plugin in direnv elixir erlang neovim nodejs python rust; do
+  for plugin in direnv elixir erlang rust; do
     asdf plugin add $plugin
   done
 }
@@ -49,53 +45,22 @@ function _install_brew_packages {
   brew install $BREW_PACKAGES
 }
 
-function _pin_autoconf_version {
-  brew install autoconf@2.69
-  brew link --overwrite autoconf@2.69
-}
-
-function _get_antigen {
-  curl -L git.io/antigen > ~/antigen.zsh
-}
-
-function _clone_repo {
-  git clone https://github.com/jdenen/config ~/Code/config
-}
-
 function _setup_zsh {
-  chmod u+x ~/Code/config/bin
-  for f in $(ls -a ~/Code/config/bin/dot/.*); do cp $f ~; done
+  echo "source $PWD/dot/.zshrc" >> ~/.zshrc
 }
 
-function _install_nvm {
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-  nvm use system || nvm install --lts && nvm use --lts
-}
-
-function _asdf_install {
-  (
-    cd ~/Code/config/dot
-    asdf install
-  )
-}
-
-function _install_lvim {
-  bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
-  mkdir -p ~/.config/lvim/
-  cp ~/Code/config/config.lua ~/.config/lvim/
+function _move_dot_files {
+  for f in .gitattributes .gitconfig .gitignore .tmux.conf; do
+    cp $f ~
+  done
 }
 
 _install_homebrew
 _tap_brew_casks
 _install_brew_packages
-_pin_autoconf_version
 _install_asdf_plugins
-_get_antigen
-_clone_repo
-_setup_zsh
-_install_nvm
-_asdf_install
-_install_lvim
+_setup_zshrc
+_move_dot_files
 
 # TODO: Setup GPG
 # key=$(gpg --list-secret-keys --keyid-format LONG | grep -e "^sec" | cut -d'/' -f2 | cut -d' ' -f1)
